ff9f2ba136c391886a862f7e07637e9cfd68d35c
dylan <dylangthomas1@gmail.com>
2023-04-20 19:54:55 -0600

    working on gameList

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/home/Home';
+import Game from './pages/game/Game';
 
 function App() {
+  const [gamesList, setGamesList] = useState<string[]>(['numberMemory'])
   return (
     <div className="App">
       <h1 className='center'>MiniGames</h1>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Home gamesList={gamesList} />} />
+        <Route path='/game' element={<Game />} />
       </Routes>
     </div>
   );
--- src/components/GameList.tsx
+++ src/components/GameList.tsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from 'react-router-dom'
 
 export default function GameList() {
     return (
         <ul className="gameList">
             <li><strong>Game List</strong></li>
             <hr></hr>
-            <li className="gameItem">Game 1</li>
+            <Link to=''><li className="gameItem">Game 1</li></Link>
             <li className="gameItem">Game 2</li>
             <li className="gameItem">Game 3</li>
             <li className="gameItem">Game 4</li>
