c9e29fdb000d98d3c7906fc584cd6960d76de1cc
dylan <dylangthomas1@gmail.com>
2023-05-06 19:02:03 -0600

    started on reaction test

---
+++ src/games/reactionTest/ReactionContext.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { createContext } from "vm";
+
+const init = {
+    roundNum: 1,
+    setRoundNum: (number: number) => {}
+}
+const reactionContext = createContext(init)
+
+type providerProps = {
+    children: React.ReactNode
+}
+export function ReactionProvider({children}: providerProps) {
+    const [roundNum, setRoundNum] = useState(1)
+    return (
+        <reactionContext.Provider value={
+            {roundNum,
+            setRoundNum
+        }
+        }>
+            {children}
+        </reactionContext.Provider>
+    )
+}
+
+export default reactionContext
--- src/games/reactionTest/ReactionTest.tsx
+++ src/games/reactionTest/ReactionTest.tsx
@@ -1,14 +1,31 @@
 import React, { useState } from "react";
+import { ReactionProvider } from "./ReactionContext";
 
 export default function ReactionTest() {
     //start, game, end
     const [mode, setMode] = useState('start')
+    const [press, setPress] = useState(false)
+
+    function startGame() {
+        setMode('game')
+        setTimeout(() => {
+            setPress(true)
+        }, 2000)
+    }
+
     return (
         <div className="reactionTest">
-            <h2 className="center">Reaction Test</h2>
-            {mode === 'start' && <button className="gamebtn" onClick={() => setMode('game')}>Start Game</button>}
-            {mode === 'game' && <div></div>}
-            {mode === 'end' && <div></div>}
+            <ReactionProvider>
+                <h2 className="center">Reaction Test</h2>
+                {mode === 'start' && <button className="gamebtn" onClick={startGame}>Start Game</button>}
+
+                {mode === 'game' && <div>
+                    {!press && <button>Don't Click Yet</button>}
+                    </div>}
+                    {press && <button>Click</button>}
+
+                {mode === 'end' && <div></div>}
+            </ReactionProvider>
         </div>
     )
 }
