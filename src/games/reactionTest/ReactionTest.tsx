280cf54d30b540b3579108f2678a3557c2661a1e
dylan <dylangthomas1@gmail.com>
2023-05-10 17:07:21 -0600

    reaction game working

--- src/games/reactionTest/ReactionTest.tsx
+++ src/games/reactionTest/ReactionTest.tsx
@@ -1,14 +1,11 @@
-import React, { useContext, useState } from "react";
-import reactionContext, { ReactionProvider } from "./ReactionContext";
+import React, { useState } from "react";
 
 export default function ReactionTest() {
-    const { roundNum, setRoundNum} = useContext(reactionContext)
-
-    //start, game, roundResults, end
+    const [roundNum, setRoundNum] = useState(1)
     const [mode, setMode] = useState('start')
     const [press, setPress] = useState(false)
-    const [startTime, setStartTime] = useState<number>(0)
-    const [time, setTime] = useState(0)
+    const [startTime, setStartTime] = useState(0)
+    const [scores, setScores] = useState<number[]>([])
 
     function startGame() {
         setMode('game')
@@ -30,38 +27,59 @@ export default function ReactionTest() {
 
     function handleClick() {
         const elapsed = performance.now() - startTime
-        setTime(elapsed)
+        const decimal = elapsed.toFixed(2)
+        const num = parseFloat(decimal)
+        setScores(prevState => {
+            return [
+                ...prevState,
+                num
+            ]
+        })
         setMode('roundResults')
     }
 
     function nextGame() {
         setPress(false)
-        setRoundNum((prevState):number => {
-            return prevState + 1
-        })
+        setRoundNum(prevState => prevState + 1)
         startGame()
     }
-    console.log(roundNum)
+
+    function results() {
+        setMode('end')
+    }
 
     return (
         <div className="reactionTest">
-            <ReactionProvider>
                 <h2 className="center">Reaction Test</h2>
                 {mode === 'start' && <button className="gamebtn" onClick={startGame}>Start Game</button>}
 
                 {mode === 'game' && <div>
+                    <h3>Round {roundNum}/5</h3>
                     {!press && <button className="early" onClick={early}>Don't Click Yet</button>}
 
                     {press && <button className="click" onClick={handleClick}>Click</button>}
                     </div>}
 
                     {mode === 'roundResults' && <div className="roundResults">
-                            <h3 className="center">{time}</h3>
+                            <h3 className="center">{scores[roundNum - 1]}</h3>
                             {roundNum < 5 && <button onClick={nextGame}>Next Round</button>}
-                        </div>}
+                            {roundNum === 5 && <button onClick={results}>Results</button>}
+                    </div>}
 
-                {mode === 'end' && <div></div>}
-            </ReactionProvider>
+                {mode === 'end' && <div>
+                        <h2>Results</h2>
+                        <ul>
+                            <li>{scores[0]}</li>
+                            <li>{scores[1]}</li>
+                            <li>{scores[2]}</li>
+                            <li>{scores[3]}</li>
+                            <li>{scores[4]}</li>
+                        </ul>
+                        <h3>Fastest Time: {Math.min(...scores)}</h3>
+                        <h3>Average Time: {
+                            (scores[0] + scores[1] + scores[2] + scores[3] + scores[4])/5
+                        }</h3>
+                    </div>}
         </div>
     )
 }
